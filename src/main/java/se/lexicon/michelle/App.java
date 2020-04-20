8efb3ad870fb540b6240da3252be2c5ac3b28b19
michelle <michelle.johansson85@gmail.com>
2020-04-20 09:05:10 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/michelle/App.java
@@ -0,0 +1,15 @@
+package se.lexicon.michelle;
+
+import se.lexicon.michelle.ArrayExercises;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int [] intArray = {11, 23,39, 50};
+
+        ArrayExercises arr = new ArrayExercises();
+        arr.printArray(intArray);
+
+    }
+}
---
+++ src/main/java/se/lexicon/michelle/ArrayExercises.java
@@ -0,0 +1,12 @@
+package se.lexicon.michelle;
+
+public class ArrayExercises {
+
+    public static void printArray(int[] intArray){
+
+        for (int i = 0 ; i < intArray.length; i++){
+           System.out.print(intArray[i] + " ");
+        }
+    }
+
+}
