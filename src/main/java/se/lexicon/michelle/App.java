3743fcbbcebcb20f2928cb158aef52f61685597e
michelle <michelle.johansson85@gmail.com>
2020-04-20 12:38:07 +0200

    App.Java: * created an array of chars. * Added som method calls
    ArrayExercises.java:
    * printArray(char[] charArray): prints out the given arrays.
    * indexOf( int[] intArray, int number): will find and return the index of an element in the array.

--- src/main/java/se/lexicon/michelle/App.java
+++ src/main/java/se/lexicon/michelle/App.java
@@ -6,10 +6,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        int [] intArray = {11, 23,39, 50};
+        int [] intArray = new int[] {11, 23,39, 50};
+        char [] charArray = { 'J', 'A', 'V', 'A'};
 
         ArrayExercises arr = new ArrayExercises();
         arr.printArray(intArray);
+        arr.printArray(charArray);
+        System.out.println("\n" + arr.indexOf(intArray, 39));
+
+
 
     }
 }
--- src/main/java/se/lexicon/michelle/ArrayExercises.java
+++ src/main/java/se/lexicon/michelle/ArrayExercises.java
@@ -9,4 +9,18 @@ public class ArrayExercises {
         }
     }
 
+    public static void printArray(char[] charArray){
+        for (int i = 0 ; i < charArray.length; i++){
+            System.out.print(charArray[i] + " ");
+        }
+    }
+    public static int indexOf( int[] intArray, int number){
+        for (int i = 0; i < intArray.length ; i++){
+            if(intArray[i] == number){
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
