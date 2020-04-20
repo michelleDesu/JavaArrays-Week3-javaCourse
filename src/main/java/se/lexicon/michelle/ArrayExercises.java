269f2138d86bb1d9fa693254a283a72f1179bdda
michelle <michelle.johansson85@gmail.com>
2020-04-20 12:41:47 +0200

    ArrayExercises.java: * added comments, description on what the methods do

--- src/main/java/se/lexicon/michelle/ArrayExercises.java
+++ src/main/java/se/lexicon/michelle/ArrayExercises.java
@@ -2,6 +2,10 @@ package se.lexicon.michelle;
 
 public class ArrayExercises {
 
+    /**
+     * which takes in an array of type ‘int’ and print it out.
+     * @param intArray
+     */
     public static void printArray(int[] intArray){
 
         for (int i = 0 ; i < intArray.length; i++){
@@ -9,11 +13,23 @@ public class ArrayExercises {
         }
     }
 
+    /**
+     * which takes in an array of type ‘char’ and print it out.
+     * @param charArray
+     */
     public static void printArray(char[] charArray){
         for (int i = 0 ; i < charArray.length; i++){
             System.out.print(charArray[i] + " ");
         }
     }
+
+    /**
+     * will find and return the index of an element in the array.
+     * If the element doesn’t exist it returns -1.
+     * @param intArray
+     * @param number
+     * @return
+     */
     public static int indexOf( int[] intArray, int number){
         for (int i = 0; i < intArray.length ; i++){
             if(intArray[i] == number){
