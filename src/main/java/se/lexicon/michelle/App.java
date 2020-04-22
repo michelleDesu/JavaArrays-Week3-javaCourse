70b68ca4c4705265d8e1b968cbcc22b589a01121
michelle <michelle.johansson85@gmail.com>
2020-04-22 20:34:37 +0200

    App.java * removed Warnings * added runExample to remove unnecessary code in main method

--- src/main/java/se/lexicon/michelle/App.java
+++ src/main/java/se/lexicon/michelle/App.java
@@ -11,6 +11,13 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        runExamples();
+    }
+
+
+    public static void runExamples(){
+
         //creates 2 arrays one of type int and one of type char
         int [] intArray = new int[] {11, 23,39, 50};
         char [] charArray = { 'J', 'A', 'V', 'A'};
@@ -23,39 +30,31 @@ public class App
 
         String[][] countryCityValues = {
                 {
-                    "France",
-                    "Paris"
+                        "France",
+                        "Paris"
                 },
                 {
-                    "Sweden",
-                    "Stockholm"
+                        "Sweden",
+                        "Stockholm"
                 }
         };
 
-        ArrayExercises arr = new ArrayExercises();
-      /*
-        arr.printArray(intArray);
-        arr.printArray(charArray);
-        System.out.println("\n" + arr.indexOf(intArray, 39));
-        arr.sortArray(stringArray);
+        ArrayExercises.printArray(intArray);
+        ArrayExercises.printArray(charArray);
+        System.out.println("\n" + ArrayExercises.indexOf(intArray, 39));
+        ArrayExercises.sortArray(stringArray);
 
 
-        arr.copyArray(intArray);
-        arr.printTwoDimenstionalStrings(countryCityValues);
-        */
+        ArrayExercises.copyArray(intArray);
+        ArrayExercises.printTwoDimenstionalStrings(countryCityValues);
+
 
         runMultiplication();
-
     }
 
     public static void runMultiplication(){
-        ArrayExercises arr = new ArrayExercises();
-       /* int[][] multiplication = {
-                {1 , 2, 3 },
-                {1 , 2, 3 }
-        };*/
         int[][] multiplication = new int[10][10];
-        arr.initArray(multiplication);
-        arr.multiplication(multiplication);
+        ArrayExercises.initArray(multiplication);
+        ArrayExercises.multiplication(multiplication);
     }
 }
