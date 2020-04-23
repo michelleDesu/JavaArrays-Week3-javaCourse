417522cffd3d3cc503776a0a49d093e59cfa5655
michelle <michelle.johansson85@gmail.com>
2020-04-23 08:42:22 +0200

    ScannerUtil.java * added getIntFromUser(): Gets a number from user, casts: invalid input if input is not a number.
    App.java:
    * Added a while with a switch to make it a rerunnable project.

--- src/main/java/se/lexicon/michelle/App.java
+++ src/main/java/se/lexicon/michelle/App.java
@@ -2,6 +2,8 @@ package se.lexicon.michelle;
 
 //import se.lexicon.michelle.ArrayExercises;
 
+import java.util.Arrays;
+
 /**
  * @author Michelle Johansson
  *
@@ -60,6 +62,37 @@ public class App
     }
 
     public static void runReverseExample(){
+        boolean quit = false,
+                stateNumber = true;
+        int number = 0;
+        int[] numbers = new int[0];
+
+        while(!quit){
+
+            System.out.println("What du you want to do? \n" +
+                    "(a (add number) , q (quit) )");
+            String input = ScannerUtil.getUserInput();
+
+            switch (input.toLowerCase()){
+                case "q":
+                    quit = true;
+                    System.out.println("Thank you for your added numbers \n" +
+                            "The numbers you added was: ");
+                            ArrayExercises.printArray(numbers);
+                    System.out.println("\nIn reverse it will be: " +
+                            Arrays.toString(ArrayExercises.reversedArray(numbers)) );
+                    break;
+
+                case "a":
+                    number =  ScannerUtil.getInputNumber();
+                    numbers = ArrayExercises.add(numbers, number);
+                    break;
+
+                default:
+                    System.out.println("The value you typed is none of the suggested alternatives");
+
+            }
+        }
 
     }
 
--- src/main/java/se/lexicon/michelle/ScannerUtil.java
+++ src/main/java/se/lexicon/michelle/ScannerUtil.java
@@ -5,9 +5,37 @@ import java.util.Scanner;
 public class ScannerUtil {
     private static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * fetches the users input and returns a string.
+     * @return
+     */
     public static String getUserInput(){
         String userInput = scanner.nextLine();
         return userInput;
     }
+
+    //Gets a number from user
+    public static int getInputNumber(){
+        System.out.print("Write a number you want to add: ");
+        return ScannerUtil.getIntFromUser();
+    }
+
+    public static int getIntFromUser(){
+        int number = 0;
+        boolean invalidInt = true;
+
+        while (invalidInt){
+            try{
+                String numberAsString = getUserInput().trim();
+                number = Integer.parseInt(numberAsString);
+                invalidInt = false;
+            }catch(NumberFormatException e){
+                System.out.print("Invalid input. Please try again: " );
+            }
+        }
+        return number;
+    }
+
+
 
 }
